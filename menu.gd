0a9a63599000bc8005d08dded9a349be9891ac77
sealionnn <72611254+sealionnn@users.noreply.github.com>
2024-11-02 16:32:02 -0400

    Update menu.gd

--- menu.gd
+++ menu.gd
@@ -2,13 +2,21 @@ class_name Menu extends Control
 
 signal button_focused(button: BaseButton)
 signal button_pressed(button: BaseButton)
+signal closed()
 
 @export var auto_wrap: bool = true
+@export var buttons_container: Container = null
+@export var hide_on_focus_exit: bool = false
 
 var index: int = 0
 var exiting: bool = false
 
 func _ready() -> void:
+	if !buttons_container:
+		queue_free()
+		print("No buttons container set on menu.")
+		return
+		
 	tree_exiting.connect(_on_tree_exiting)
 	
 	# Connect to buttons.
@@ -24,74 +32,81 @@ func _ready() -> void:
 	if !auto_wrap:
 		return
 	
-	var _class: String = get_class()
+	var _class: String = buttons_container.get_class()
 	var buttons: Array = get_buttons()
 	var use_this_on_grid_containers: bool = false # hot fix
 	
-	if use_this_on_grid_containers and get("columns"): # GridContainer
-		var top_row: Array = []
-		var bottom_row: Array = []
-		var cols: int = self.columns
-		var rows: int = round(buttons.size() / cols)
-		var btm_range: Array = [rows * cols - cols, rows * cols - 1]
-#		var btm_range: Array = [rows * (cols - 1) + 1, rows * cols]
+	match _class:
+		"GridContainer":
+			if use_this_on_grid_containers:
+				var top_row: Array = []
+				var bottom_row: Array = []
+				var cols: int = self.columns
+				var rows: int = round(buttons.size() / cols)
+				var btm_range: Array = [rows * cols - cols, rows * cols - 1]
+		#		var btm_range: Array = [rows * (cols - 1) + 1, rows * cols]
 
-#		print(btm_range)
+		#		print(btm_range)
 
-		#if clear_first:
-			#for button in buttons:
-				#button.focus_neighbor_top = null
-				#button.focus_neighbor_bottom = null
+				#if clear_first:
+					#for button in buttons:
+						#button.focus_neighbor_top = null
+						#button.focus_neighbor_bottom = null
 
-		# Get top and bottom rows of buttons.
-		for x in cols:
-#			print(buttons[x].text)
-			top_row.append(buttons[x])
-		for x in range(btm_range[0], btm_range[1] + 1):
-#			print(x)
-			if x > buttons.size():
-#				print(buttons[x - cols].text)
-				bottom_row.append(buttons[x - cols])
-				continue
-#			print(buttons[x].text)
-			bottom_row.append(buttons[x])
+				# Get top and bottom rows of buttons.
+				for x in cols:
+		#			print(buttons[x].text)
+					top_row.append(buttons[x])
+				for x in range(btm_range[0], btm_range[1] + 1):
+		#			print(x)
+					if x > buttons.size():
+		#				print(buttons[x - cols].text)
+						bottom_row.append(buttons[x - cols])
+						continue
+		#			print(buttons[x].text)
+					bottom_row.append(buttons[x])
 
-		# Change their focus neighbors accordingly.
-		for x in cols:
-			var top_button: BaseButton = top_row[x]
-			var bottom_button: BaseButton = bottom_row[x]
-#			print(top_button)
-#			print(bottom_button)
-			if top_button == bottom_button:
-				continue
+				# Change their focus neighbors accordingly.
+				for x in cols:
+					var top_button: BaseButton = top_row[x]
+					var bottom_button: BaseButton = bottom_row[x]
+		#			print(top_button)
+		#			print(bottom_button)
+					if top_button == bottom_button:
+						continue
+					top_button.focus_neighbor_top = bottom_button.get_path()
+					bottom_button.focus_neighbor_bottom = top_button.get_path()
+
+				# Repeat for left and right columns.	
+				for i in range(0, buttons.size(), cols):
+					var left_button: BaseButton = buttons[i]
+					var right_button: BaseButton = buttons[i + cols - 1]
+		#			print(left_button, "...", right_button)
+					left_button.focus_neighbor_left = right_button.get_path()
+					right_button.focus_neighbor_right = left_button.get_path()
+		"VBoxContainer":
+			var top_button: BaseButton = buttons.front()
+			var bottom_button: BaseButton = buttons.back()
 			top_button.focus_neighbor_top = bottom_button.get_path()
 			bottom_button.focus_neighbor_bottom = top_button.get_path()
-
-		# Repeat for left and right columns.	
-		for i in range(0, buttons.size(), cols):
-			var left_button: BaseButton = buttons[i]
-			var right_button: BaseButton = buttons[i + cols - 1]
-#			print(left_button, "...", right_button)
-			left_button.focus_neighbor_left = right_button.get_path()
-			right_button.focus_neighbor_right = left_button.get_path()
-	elif _class.begins_with("VBox"):
-		var top_button: BaseButton = buttons.front()
-		var bottom_button: BaseButton = buttons.back()
-		top_button.focus_neighbor_top = bottom_button.get_path()
-		bottom_button.focus_neighbor_bottom = top_button.get_path()
-	elif _class.begins_with("HBox"):
-		var first_button: BaseButton = buttons.front()
-		var last_button: BaseButton = buttons.back()
-		first_button.focus_neighbor_left = last_button.get_path()
-		last_button.focus_neighbor_right = first_button.get_path()
+		"HBoxContainer":
+			var first_button: BaseButton = buttons.front()
+			var last_button: BaseButton = buttons.back()
+			first_button.focus_neighbor_left = last_button.get_path()
+			last_button.focus_neighbor_right = first_button.get_path()
 		
 	button_enable_focus(false)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		button_enable_focus(false)
+		closed.emit()
 
 func get_buttons_count() -> int:
 	return get_child_count()
 
 func get_buttons() -> Array:
-	return get_children()
+	return buttons_container.get_children()
 
 func connect_to_buttons(target: Object, _name: String = name) -> void:
 	var callable: Callable = Callable()
@@ -104,6 +119,9 @@ func button_enable_focus(on: bool) -> void:
 	var mode: FocusMode = FocusMode.FOCUS_ALL if on else FocusMode.FOCUS_NONE
 	for button in get_buttons():
 		button.set_focus_mode(mode)
+	
+	if hide_on_focus_exit:	
+		visible = on
 
 func button_focus(n: int = index) -> void:
 	await get_tree().process_frame
